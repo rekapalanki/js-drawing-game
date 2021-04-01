c0925a3af5ed051edeb0ba0943b8055a467de292
Reka Palanki <reka.palanki@gmail.com>
2021-04-01 12:51:56 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// Grab elements
+
+const canvas = document.querySelector('#drawing-game');
+const ctx = canvas.getContext('2d');
+
+// Setup canvas 
+
+ctx.lineCap = 'round';
+ctx.lineJoin = 'round';
+ctx.lineWidth = 10;
+
+ctx.beginPath();
+ctx.moveTo(200,200);
+ctx.lineTo(200,200);
+ctx.stroke();
+
+// Create draw function 
+
+// Create Arrow event handler
+
+// Add event listener
+
+// Create clear function and click event listener
