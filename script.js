2bae95a986cc879ffa88c3994297f64fd71cf684
Reka Palanki <reka.palanki@gmail.com>
2021-04-01 13:30:29 +0100

    changing hue, adding header, changing background

--- script.js
+++ script.js
@@ -2,34 +2,45 @@
 
 const canvas = document.querySelector('#drawing-game');
 const ctx = canvas.getContext('2d');
+const shake = document.querySelector('.shake');
 const LINE_WIDTH = 10;
 
 // Setup canvas 
 
 const {width, height} = canvas;
-let [x, y] = [
+let [x, y, hue] = [
   Math.floor(Math.random()*width),
   Math.floor(Math.random()*height),
+  0
 ]
 
 ctx.lineCap = 'round';
 ctx.lineJoin = 'round';
 ctx.lineWidth = LINE_WIDTH;
 
-ctx.fillStyle = '#000';
-ctx.fillRect(0, 0, width, height);
+const rect = () => {
+  ctx.fillStyle = '#000';
+  ctx.fillRect(0, 0, width, height)
+}
 
-ctx.beginPath();
-ctx.strokeStyle = '#fff';
-ctx.moveTo(x, y);
-ctx.lineTo(x, y);
-ctx.stroke();
+const startPoint = () =>  {
+  ctx.beginPath();
+  ctx.strokeStyle = `hsl(${hue}, 100%, 85%)`;
+  ctx.moveTo(x, y);
+  ctx.lineTo(x, y);
+  ctx.stroke();
+}
+
+rect();
+startPoint();
 
 // Create draw function 
 
 const draw = ({key}) => {
   console.log(key);
+  hue += 3;
   ctx.beginPath();
+  ctx.strokeStyle = `hsl(${hue}, 100%, 85%)`;
   ctx.moveTo(x, y);
   switch (key) {
     case 'ArrowUp':
@@ -70,3 +81,9 @@ const arrowHandler = (event) => {
 window.addEventListener('keydown', arrowHandler)
 
 // Create clear function and click event listener
+
+const clickHandler = (event) => {
+  console.log(event);
+}
+
+shake.addEventListener('click', clickHandler);
